64a375a0a824741449fef7ce3eb9c31c180792e5
Romel Santoral <ctechnology24@gmail.com>
2022-03-31 23:27:49 +0800

    Create `create_user_controller` function

--- components/controllers/controller.py
+++ components/controllers/controller.py
@@ -1,9 +1,26 @@
 from flask import jsonify
-from components.database.users import login_user
+from components.database import users as usr
 
 
 def login_controller(payload):
     try:
-        return login_user(payload['username'], payload['password'])
+        return usr.login_user(payload['username'], payload['password'])
     except:
         return jsonify({'error': 'Incorrect username or password.'}), 401
+
+
+def create_user_controller(payload):
+    if not payload.get('username'):
+        return jsonify({'error': 'Username is required.'}), 400
+    if not payload.get('email'):
+        return jsonify({'error': 'Email is required.'}), 400
+    if not payload.get('password'):
+        return jsonify({'error': 'Password is required.'}), 400
+    # Scheduler or Doctor
+    if not payload.get('type'):
+        return jsonify({'error': 'User type is required.'}), 400
+    if payload['type'].lower() == 'doctor':
+        if not payload.get('available'):
+            return jsonify({'error': 'Availability is required.'}), 400
+    return usr.create_user(payload)
+    
