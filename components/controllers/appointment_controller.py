245e9f03b688082ecb65393cc302ab1a85057eab
Romel Santoral <ctechnology24@gmail.com>
2022-04-02 03:55:26 +0800

    Reduce restriction

--- components/controllers/appointment_controller.py
+++ components/controllers/appointment_controller.py
@@ -22,28 +22,19 @@ def create_appointment_controller(payload):
     return apt.create_appointment(payload)
 
 
-def edit_appointment_controller(payload):
+def update_appointment_controller(payload):
     if apt.is_appointment_accepted(payload['id']):
         return jsonify({
             'error': 'Changes not allowed. Appointment already accepted by physician.'}), 405
 
     apt_start = payload.get('start')
     apt_end = payload.get('end')
-    payload['accepted'] = False
-    if not apt_start:
-        return jsonify({'error': 'Start time and date is required.'}), 400
-    if not apt_end:
-        return jsonify({'error': 'End time and date is required.'}), 400
-    if not payload.get('assigned_to'):
-        payload['assigned_to'] = None
-    if not payload.get('accepted'):
-        payload['accepted'] = False
 
-    if not datetime_validator(apt_start):
+    if apt_start and not datetime_validator(apt_start):
         return jsonify({'error': 'Start time and date is invalid.'}), 400
-    if not datetime_validator(apt_end):
+    if apt_end and not datetime_validator(apt_end):
         return jsonify({'error': 'End time and date is invalid.'}), 400
-    if apt_start >= apt_end:
+    if apt_start and apt_end and apt_start >= apt_end:
         return jsonify({'error': 'End date and time should be later than start.'}), 400
 
     return apt.update_appointment(payload)
