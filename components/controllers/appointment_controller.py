be97af7a4cc463dbafdac301fadb8185ac079772
Romel Santoral <ctechnology24@gmail.com>
2022-04-02 03:08:09 +0800

    Create appointments controllers

---
+++ components/controllers/appointment_controller.py
@@ -0,0 +1,63 @@
+from flask import jsonify
+from components.database import appointments as apt
+from components.utils.helpers import datetime_validator
+
+
+def create_appointment_controller(payload):
+    apt_start = payload.get('start')
+    apt_end = payload.get('end')
+    payload['assigned_to'] = None
+    payload['accepted'] = False
+    if not apt_start:
+        return jsonify({'error': 'Start time and date is required.'}), 400
+    if not apt_end:
+        return jsonify({'error': 'End time and date is required.'}), 400
+    if not datetime_validator(apt_start):
+        return jsonify({'error': 'Start time and date is invalid.'}), 400
+    if not datetime_validator(apt_end):
+        return jsonify({'error': 'End time and date is invalid.'}), 400
+    if apt_start >= apt_end:
+        return jsonify({'error': 'End date and time should be later than start.'}), 400
+
+    return apt.create_appointment(payload)
+
+
+def edit_appointment_controller(payload):
+    # payload should contain the appointment id from frontend
+    apt_start = payload.get('start')
+    apt_end = payload.get('end')
+    
+    payload['accepted'] = False
+    if not apt_start:
+        return jsonify({'error': 'Start time and date is required.'}), 400
+    if not apt_end:
+        return jsonify({'error': 'End time and date is required.'}), 400
+    if not payload.get('assigned_to'):
+        payload['assigned_to'] = None
+    if not payload.get('accepted'):
+        payload['accepted'] = False
+    if not datetime_validator(apt_start):
+        return jsonify({'error': 'Start time and date is invalid.'}), 400
+    if not datetime_validator(apt_end):
+        return jsonify({'error': 'End time and date is invalid.'}), 400
+    if apt_start >= apt_end:
+        return jsonify({'error': 'End date and time should be later than start.'}), 400
+
+    return apt.update_appointment(payload)
+
+
+def search_appointment_controller(payload):
+    apt_start = payload.get('start')
+    apt_end = payload.get('end')
+    assigned_to = payload.get('assigned_to')
+    accepted = payload.get('accepted')
+    print(payload)
+    if apt_start and not datetime_validator(apt_start):
+        return jsonify({'error': 'Start time and date is invalid.'}), 400
+    if apt_end and not datetime_validator(apt_end):
+        return jsonify({'error': 'End time and date is invalid.'}), 400
+    return apt.search_appointment(
+                    start=apt_start,
+                    end=apt_end,
+                    assigned_doctor=assigned_to,
+                    accepted=accepted)
