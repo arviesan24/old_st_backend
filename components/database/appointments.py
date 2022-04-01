c0c61cba54b7a1c494092dfcc917708e7223ca79
Romel Santoral <ctechnology24@gmail.com>
2022-04-02 03:07:22 +0800

    Create `search_appointment` db func

---
+++ components/database/appointments.py
@@ -0,0 +1,57 @@
+import random
+import string
+import time
+from flask import jsonify
+from components.utils import redis as rs
+
+
+def create_appointment(payload):
+    collection = 'appointments'
+    random_str = ''.join(random.choices(string.ascii_lowercase, k=3))
+    key = f'{time.time()}{random_str}'
+    id = f'{collection}_{key}'
+    payload['id'] = id
+    data = payload
+    rs.create_data(collection, key, data)
+    return jsonify({'status': 'Appointment created.'})
+
+
+def update_appointment(payload):
+    collection = 'appointments'
+    key = payload['id'].split('_')[1]
+    data = payload
+    rs.create_data(collection, key, data)
+    return jsonify({'status': 'Appointment updated.'})
+
+def assign_appointment(appointment, doctor):
+    collection = 'appointments'
+    key = appointment.split('_')[1]
+    data = rs.read_data(collection, key)
+    data['assigned_to'] = doctor
+    rs.create_data(collection, key, data)
+    return jsonify({'status': f'Appointment assigned to the doctor.'})
+
+
+def list_appointments():
+    records = rs.get_all_from_collection('appointments')
+    return records
+
+
+def search_appointment(start=None, end=None, assigned_doctor=None, accepted=None):
+    appointments = list_appointments()
+    print(appointments)
+    if start:
+        appointments = [item for item in appointments if item['start']==start]
+    if end:
+        appointments = [item for item in appointments if item['end']==end]
+    if assigned_doctor:
+        appointments = [item for item in appointments if item['assigned_to']==assigned_doctor]
+    if accepted:
+        appointments = [item for item in appointments if item['accepted']==accepted]
+    return jsonify({'data': appointments})
+
+
+
+
+
+# TODO: Complete appointment database then create controllers for it
