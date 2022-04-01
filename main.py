5e893f649d4b7f173fc19e77821851dbfbd0da1a
Romel Santoral <ctechnology24@gmail.com>
2022-04-01 20:01:56 +0800

    Register appointment blueprint

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
 from blueprints.users import blueprint as user_blueprint
+from blueprints.appointments import blueprint as appointment_blueprint
 
 
 app = Flask(__name__)
 app.register_blueprint(user_blueprint)
+app.register_blueprint(appointment_blueprint)
 
 
 @app.route("/")
