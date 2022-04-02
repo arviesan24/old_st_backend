4f5d1aa6e511f4be0a5552b067634ffdc8198a91
Romel Santoral <ctechnology24@gmail.com>
2022-04-02 08:32:56 +0800

    Add date in the appointment fields

--- components/controllers/appointment_controller.py
+++ components/controllers/appointment_controller.py
@@ -1,55 +1,68 @@
 from flask import jsonify
 from components.database import appointments as apt
-from components.utils.helpers import datetime_validator
+from components.utils.helpers import date_validator, time_validator
 
 
 def create_appointment_controller(payload):
+    apt_date = payload.get('date')
     apt_start = payload.get('start')
     apt_end = payload.get('end')
     payload['assigned_to'] = None
     payload['accepted'] = False
+    if not apt_date:
+        return jsonify({"error": "Date is required."}), 400
     if not apt_start:
-        return jsonify({'error': 'Start time and date is required.'}), 400
+        return jsonify({"error": "Start time is required."}), 400
     if not apt_end:
-        return jsonify({'error': 'End time and date is required.'}), 400
-    if not datetime_validator(apt_start):
-        return jsonify({'error': 'Start time and date is invalid.'}), 400
-    if not datetime_validator(apt_end):
-        return jsonify({'error': 'End time and date is invalid.'}), 400
+        return jsonify({"error": "End time is required."}), 400
+    if not date_validator(apt_date):
+        return jsonify({"error": "Date is invalid. Format('YYYY-MM-DD')"}), 400
+    if not time_validator(apt_start):
+        return jsonify({"error": "Start time is invalid. Format('HH:MM')"}), 400
+    if not time_validator(apt_end):
+        return jsonify({"error": "End time is invalid. Format('HH:MM')"}), 400
     if apt_start >= apt_end:
-        return jsonify({'error': 'End date and time should be later than start.'}), 400
+        return jsonify({"error": "End time should be later than start time."}), 400
 
     return apt.create_appointment(payload)
 
 
 def update_appointment_controller(payload):
-    if apt.is_appointment_accepted(payload['id']):
-        return jsonify({
-            'error': 'Changes not allowed. Appointment already accepted by physician.'}), 405
-
+    apt_id = payload.get('id')
+    apt_date = payload.get('date')
     apt_start = payload.get('start')
     apt_end = payload.get('end')
 
-    if apt_start and not datetime_validator(apt_start):
-        return jsonify({'error': 'Start time and date is invalid.'}), 400
-    if apt_end and not datetime_validator(apt_end):
-        return jsonify({'error': 'End time and date is invalid.'}), 400
+    if not apt_id:
+        return jsonify({"error": "Appointment ID missing."}), 400
+    if apt_date and not date_validator(apt_date):
+        return jsonify({"error": "Date is invalid. Format('YYYY-MM-DD')"}), 400
+    if apt_start and not time_validator(apt_start):
+        return jsonify({"error": "Start time is invalid. Format('HH:MM')"}), 400
+    if apt_end and not time_validator(apt_end):
+        return jsonify({"error": "End time is invalid. Format('HH:MM')"}), 400
     if apt_start and apt_end and apt_start >= apt_end:
-        return jsonify({'error': 'End date and time should be later than start.'}), 400
+        return jsonify({"error": "End time should be later than start time."}), 400
+
+    if apt.is_appointment_accepted(payload['id']):
+        return jsonify({
+            "error": "Changes not allowed. Appointment already accepted by physician."}), 405
 
     return apt.update_appointment(payload)
 
 
 def search_appointment_controller(payload):
+    apt_date = payload.get('date')
     apt_start = payload.get('start')
     apt_end = payload.get('end')
     assigned_to = payload.get('assigned_to')
     accepted = payload.get('accepted')
-    print(payload)
-    if apt_start and not datetime_validator(apt_start):
-        return jsonify({'error': 'Start time and date is invalid.'}), 400
-    if apt_end and not datetime_validator(apt_end):
-        return jsonify({'error': 'End time and date is invalid.'}), 400
+    if apt_date and not date_validator(apt_date):
+        return jsonify({"error": "Date is invalid. Format('YYYY-MM-DD')"}), 400
+    if apt_start and not time_validator(apt_start):
+        return jsonify({"error": "Start time is invalid. Format('HH:MM')"}), 400
+    if apt_end and not time_validator(apt_end):
+        return jsonify({"error": "End time is invalid. Format('HH:MM')"}), 400
     return apt.search_appointment(
                     start=apt_start,
                     end=apt_end,
