788bd5b4c36e3dd20634a71093461a55ddd4a454
Romel Santoral <ctechnology24@gmail.com>
2022-04-02 19:24:15 +0800

    Create change doc availability endpoint

--- blueprints/users.py
+++ blueprints/users.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from components.controllers import user_controller as user_ctrl
+from components.database.users import check_user_type
+from components.utils.jwt import check_token
 
 
 blueprint = Blueprint('users', __name__)
@@ -13,3 +15,12 @@ def login():
 @blueprint.route("/create-user", methods=['POST'])
 def create_user():
     return user_ctrl.create_user_controller(request.get_json())
+
+
+@blueprint.route("/change-doctor-availability", methods=['POST'])
+@check_token
+def doctor_availability(user):
+    user_type = check_user_type(user['userID'])
+    if user_type == 'scheduler':
+        return user_ctrl.change_doctor_availability_controller(request.get_json())
+    return jsonify({'error': 'Unauthorized access.'}), 401
