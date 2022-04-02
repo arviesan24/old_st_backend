f53017f4f7ef7b7eea6134f4874916cc865a0103
Romel Santoral <ctechnology24@gmail.com>
2022-04-02 19:28:58 +0800

    Create `change_doctor_availability_controller`

--- components/controllers/user_controller.py
+++ components/controllers/user_controller.py
@@ -23,4 +23,18 @@ def create_user_controller(payload):
         if payload.get('available') == None:
             return jsonify({'error': 'Availability is required.'}), 400
     return usr.create_user(payload)
-    
+
+
+def change_doctor_availability_controller(payload):
+    availability = payload.get('available')
+    if availability is None:
+        return jsonify({'error': 'Availability is required.'}), 400
+    if availability is not True and availability is not False:
+        return jsonify({'error': 'Availability should be a boolean.'}), 400
+    doctor = payload.get('doctor')
+    if doctor is None:
+        return jsonify({'error': 'Doctor is required.'}), 400
+    doctor_on_list = usr.search_doctor(doctor)
+    if not doctor_on_list:
+        return jsonify({'error': 'Doctor is not on the list.'}), 400
+    return usr.change_doctor_availability(doctor, availability)
