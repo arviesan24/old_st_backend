acd22b84e842313974d71dd7feaa01c92ad22ec8
Romel Santoral <ctechnology24@gmail.com>
2022-04-03 12:11:24 +0800

    Create email template file

---
+++ components/utils/email_template.py
@@ -0,0 +1,16 @@
+template_text = """
+Hi {doctor},
+
+A new appointment has been assigned to you with the following details.
+
+DATE: {date}
+START TIME: {start}
+END TIME: {end}
+PATIENT NAME: {patient}
+ACCEPTED: {accepted}
+COMMENTS: {comment}
+
+To accept the scheduled appointment, just login to your account.
+
+See you in the clinic.
+"""
