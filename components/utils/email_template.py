92b7f8866a9121a8a42bc4ae52274e8691a620fd
Romel Santoral <ctechnology24@gmail.com>
2022-04-03 14:16:19 +0800

    Create template for updated appointment

--- components/utils/email_template.py
+++ components/utils/email_template.py
@@ -1,7 +1,24 @@
-template_text = """
+new_appointment = """
 Hi {doctor},
 
 A new appointment has been assigned to you with the following details.
+
+DATE: {date}
+START TIME: {start}
+END TIME: {end}
+PATIENT NAME: {patient}
+ACCEPTED: {accepted}
+COMMENTS: {comment}
+
+To accept the scheduled appointment, just login to your account.
+
+See you in the clinic.
+"""
+
+updated_appointment = """
+Hi {doctor},
+
+An appointment assigned to you has been updated. Please check the details below.
 
 DATE: {date}
 START TIME: {start}
