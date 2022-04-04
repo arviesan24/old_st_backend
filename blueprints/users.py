aca034fbfb6201b8ecbca2f95374003a8d0797f2
Romel Santoral <ctechnology24@gmail.com>
2022-04-05 04:18:12 +0800

    Additional router fix

--- blueprints/users.py
+++ blueprints/users.py
@@ -7,17 +7,17 @@ from components.utils.jwt import check_token
 blueprint = Blueprint('users', __name__)
 
 
-@blueprint.route("users/login", methods=['POST'])
+@blueprint.route("/users/login", methods=['POST'])
 def login():
     return user_ctrl.login_controller(request.get_json())
 
 
-@blueprint.route("users/create", methods=['POST'])
+@blueprint.route("/users/create", methods=['POST'])
 def create_user():
     return user_ctrl.create_user_controller(request.get_json())
 
 
-@blueprint.route("users/change-doctor-availability", methods=['POST'])
+@blueprint.route("/users/change-doctor-availability", methods=['POST'])
 @check_token
 def doctor_availability(user):
     if user:
@@ -25,7 +25,7 @@ def doctor_availability(user):
     return jsonify({'error': 'Unauthorized access.'}), 401
 
 
-@blueprint.route("users/authenticate")
+@blueprint.route("/users/authenticate")
 @check_token
 def authenticate(user):
     user_type = check_user_type(user['userID'])
