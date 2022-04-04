4ba9a2b02f6309500567c8858bda55b3031391f5
Romel Santoral <ctechnology24@gmail.com>
2022-04-05 04:03:02 +0800

    Add endpoint for constant user validation

--- blueprints/users.py
+++ blueprints/users.py
@@ -7,19 +7,28 @@ from components.utils.jwt import check_token
 blueprint = Blueprint('users', __name__)
 
 
-@blueprint.route("/login", methods=['POST'])
+@blueprint.route("users/login", methods=['POST'])
 def login():
     return user_ctrl.login_controller(request.get_json())
 
 
-@blueprint.route("/create-user", methods=['POST'])
+@blueprint.route("users/create", methods=['POST'])
 def create_user():
     return user_ctrl.create_user_controller(request.get_json())
 
 
-@blueprint.route("/change-doctor-availability", methods=['POST'])
+@blueprint.route("users/change-doctor-availability", methods=['POST'])
 @check_token
 def doctor_availability(user):
     if user:
         return user_ctrl.change_doctor_availability_controller(user, request.get_json())
+    return jsonify({'error': 'Unauthorized access.'}), 401
+
+
+@blueprint.route("/users/authenticate")
+@check_token
+def authenticate(user):
+    user_type = check_user_type(user['userID'])
+    if user:
+        jsonify({'status': 'OK', 'type': user_type}), 200
     return jsonify({'error': 'Unauthorized access.'}), 401
